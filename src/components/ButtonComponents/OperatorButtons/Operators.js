e3afec12d1e36932f652cd00775674940719312e
Pere <pere.solaclaver@gmail.com>
2019-09-10 14:27:15 +0100

    Set state for each of the wrapper component files: numbers, operators, specials

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import {operators} from './data';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
+console.log(numbers)
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operators, setOperatorState] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
